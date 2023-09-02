1c17b026a9501658d0d1ad2eabca2a6d4d7087d6
n-salaun <107122757+n-salaun@users.noreply.github.com>
2023-09-02 22:00:54 +0200

    Add file via upload
    First Pseudo code of the project

---
+++ monte_carlo.py
@@ -0,0 +1,34 @@
+import numpy as np
+import random
+
+###Functions
+
+def energy_calculator():
+    """Function to calculate the total energy of the protein"""
+    return(energy)
+
+def check_available_moves():
+    return()
+
+#Movesets
+def corner_move():
+    return()
+    
+def crankshaft_move():
+    return()
+
+def pull_move():
+    return()
+
+###Main
+
+#Select a random amino acid
+
+#check possible moves
+
+#select a move
+
+#apply the move
+
+#check if the molecule is more stable, or accepted
+
